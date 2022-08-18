55c91f0b16beb1a0f42d739b215d67971f21f96a
Your Name <you@example.com>
2022-08-18 14:21:16 -0600

    updates to html and js files

--- Cupcakes.js
+++ Cupcakes.js
@@ -5,21 +5,24 @@ const cupcakeList = [
     {
         id: 0,
         flavor: "Chocolate",
-        filling: "Raspberry"
+        filling: "Raspberry",
+        frosting: "Buttercream"
     },
     {
         id: 1,
         flavor: "Lemon",
-        filling: "Lemon"
+        filling: "Lemon",
+        frosting: "Whipped Cream"
     },
 ]
 
+//RENDERING
 $(() => {
     renderCupcakeList()
 })
 
 //container piece//
-const $cupcakeContainer = $("cupcakes-container")
+const $cupcakeContainer = $("#cupcakes-container")
 
 
 //function to take the data, loop it, render a card for each, and put it into the Cupcake container*//
@@ -30,19 +33,70 @@ function renderCupcakeList() {
     $cupcakeContainer.append(cupcakeList.map(cupcake => renderCupcake(cupcake)))
 }
 
-//function to reder individual cupcakes//
+//function to reder individual cupcakes with edit and delete buttons//
 function renderCupcake(cupcake) {
     return $("<div/>").addClass("card mt-4").append(
         $("<div/>").addClass("card-body").append(
-            $("<h5/>").addClass("card-title").text(cupcake.flavor)
+            $("<h5/>").addClass("card-title").text('Cupcake: ' + cupcake.flavor + ' with ' + cupcake.filling + ' filling & ' + cupcake.frosting + ' frosting.'),
+            $("<button/>").addClass("btn btn-primary me-5").text("Edit").on("click", () => onEdit(cupcakeId)),
+            
+            //using jQuery to get the event listener-$button.on("click", function). In vanilla JS it would be theButton.addEventListener("click", function).
+            $("<button/>").addClass("btn btn-danger me-5").text("Delete").on("click", () => onDeleteClick(cupcake.id)),
         )
     )
 }
 
-/* <div class="card mt-4" id="card">
-            <div class="card-body">
-                <h5 class="card-title">Chocolate with Rapsberry Filling</h5>
-                <button class="btn btn-primary">Edit</button>
-                <button class="btn btn-danger">Delete</button>
-            </div>
-        </div> */
+//EVENT LISTENERS
+
+const cupcakeModal = new bootstrap.Modal("#new-cupcake-modal")
+const $cupcakeModalTitle = $("cupcake-modal-title")
+const $flavorInput = $("#flavor-input")
+const $fillingInput = $("#filling-input")
+const $frostingInput = $("#frosting-input")
+
+function onNewCupcakeClick() {
+    //opening the modal
+    cupcakeModal.show();
+
+    //clearing the form. Usually would do $nameInput.text = " "; but using a method.
+    $flavorInput.val("");
+    $fillingInput.val("");
+    $frostingInput.val("");
+    
+}
+
+function onEdit(cupcakeId){
+    //selecting the right cupcake to edit
+    const cupcake = cupcakeList.find(cupcake => cupcake.id === cupcakeId);
+    //opening the modal
+    cupcakeModal.show();
+    //change title of the modal
+    $cupcakeModalTitle.text("Edit")
+
+    //put current cupcake data in form
+    $flavorInput.val(cupcake.flavor);
+    $fillingInput.val(cupcake.filling);
+    $frostingInput.val(cupcake.frosting);
+}
+
+function onSaveCreate() {
+    //get the input from the modal and create new cupcake and add it to list
+    cupcakeList.push({
+        id: cupcakeList[cupcakeList.length -1].id + 1,
+        flavor: $flavorInput.val(),
+        filling: $fillingInput.val(),
+        frosting: $frostingInput.val()
+    })
+    
+    //rereder list
+    renderCupcakeList();
+    
+    //close the modal
+    cupcakeModal.hide();
+}
+
+function onDeleteClick (cupcakeId) {
+    const indexToDelete = cupcakeList.findIndex(cupcake => cupcake.id === cupcakeId)
+    cupcakeList.splice(indexToDelete, 1);
+    renderCupcakeList();
+}
